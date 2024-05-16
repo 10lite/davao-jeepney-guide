26041f31ffe62a415be5de38a01335a63c68abf7
10lite <rcpaderna@up.edu.ph>
2024-05-16 09:23:54 +0800

    feat: Commute Details Badges

---
+++ app/travel/components/commute-details.tsx
@@ -0,0 +1,12 @@
+// Components
+import { Badge } from "@/components/ui/badge"
+
+export default function CommuteDetails() {
+  return (
+    <div className="flex gap-2 mt-2">
+      <Badge>22.22 Kilometers</Badge>
+      <Badge>2 Rides</Badge>
+      <Badge>~25 PHP</Badge>  
+    </div>  
+  )
+}
--- app/travel/components/map-container.tsx
+++ app/travel/components/map-container.tsx
@@ -1,4 +1,5 @@
 // Components
+import { Badge } from "@/components/ui/badge"
 import {
   Card,
   CardContent,
@@ -7,13 +8,18 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import CommuteDetails from "./commute-details"
 
 export default function MapContainer() {
   return (
     <Card className="w-full sm:w-[60%] sm:p-6 border-gray-400">
       <CardHeader className="sm:pt-0">
-        <CardTitle>Traveling from -- to --</CardTitle>
-        <CardDescription>--.-- Kilometers | - Jeepney Rides</CardDescription>
+        <CardTitle>
+          Traveling from <span className="underline">Source</span> to <span className="underline">Destination</span>
+        </CardTitle>
+        <CardDescription>
+          <CommuteDetails />
+        </CardDescription>
       </CardHeader>
       <CardContent className="p-0 sm:px-6">
         <div className="h-[20rem] bg-gray-200 rounded-b-lg"></div>

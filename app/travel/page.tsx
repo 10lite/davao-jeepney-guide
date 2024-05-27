d5d6fb9d6a1125d811f4066f6d5921a60151ebe1
aspactores <anakin.pactores@gmail.com>
2024-05-28 05:50:52 +0800

    feat: display specific jeepney transfer instruction

--- app/travel/page.tsx
+++ app/travel/page.tsx
@@ -16,6 +16,23 @@ import MapContainer from "./components/map-container";
 import { useState } from "react";
 import { useJsApiLoader } from "@react-google-maps/api";
 import AutocompleteInput from "@/components/AutocompleteInput";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
+import DavaoWeather from "@/components/DavaoWeather";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Travel() {
   const [source, setSource] = useState<google.maps.places.PlaceResult>();
@@ -26,6 +43,8 @@ export default function Travel() {
   const [distanceResponse, setDistanceResponse] =
     useState<google.maps.DistanceMatrixResponse>();
   const [selectedRoute, setSelectedRoute] = useState<number>();
+  const [instructions, setInstructions] =
+    useState<google.maps.DirectionsStep[]>();
 
   const { isLoaded, loadError } = useJsApiLoader({
     id: "google-maps",
@@ -50,8 +69,14 @@ export default function Travel() {
       });
       // Sort routes by duration
       results.routes.sort((a, b) => {
-        const durationA = a.legs.reduce((total, leg) => total + leg.duration.value, 0);
-        const durationB = b.legs.reduce((total, leg) => total + leg.duration.value, 0);
+        const durationA = a.legs.reduce(
+          (total, leg) => total + leg.duration!.value,
+          0
+        );
+        const durationB = b.legs.reduce(
+          (total, leg) => total + leg.duration!.value,
+          0
+        );
         return durationA - durationB;
       });
       setDirectionResponse(results);
@@ -69,71 +94,152 @@ export default function Travel() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col sm:flex-row justify-center items-center gap-12 sm:pt-12 p-6 bg-gray-100">
+    <main className="flex min-h-screen flex-col sm:flex-row justify-center items-center gap-12 sm:pt-12 p-6 bg-gray-100 relative">
       {!directionResponse ? (
-        <Card className="border-gray-400">
-          <CardHeader className="pb-2 sm:pb-6">
-            <CardTitle>Where are you commuting to?</CardTitle>
-            <CardDescription>
-              Input your source and destination to locate jeepney routes.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="pb-4 sm:pb-6">
-            <form>
-              <div className="grid w-full items-center gap-2 sm:gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="from">From</Label>
-                  <AutocompleteInput
-                    isLoaded={isLoaded}
-                    loadError={loadError}
-                    selected={source}
-                    setSelected={setSource}
-                  />
+        <div className="flex flex-col space-y-5">
+          <Card className="border-gray-400">
+            <CardHeader className="pb-2 sm:pb-6">
+              <CardTitle>Where are you commuting to?</CardTitle>
+              <CardDescription>
+                Input your source and destination to locate jeepney routes.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="pb-4 sm:pb-6">
+              <form>
+                <div className="grid w-full items-center gap-2 sm:gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="from">From</Label>
+                    <AutocompleteInput
+                      isLoaded={isLoaded}
+                      loadError={loadError}
+                      selected={source}
+                      setSelected={setSource}
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="to">To</Label>
+                    <AutocompleteInput
+                      isLoaded={isLoaded}
+                      loadError={loadError}
+                      selected={destination}
+                      setSelected={setDestination}
+                    />
+                  </div>
                 </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="to">To</Label>
-                  <AutocompleteInput
-                    isLoaded={isLoaded}
-                    loadError={loadError}
-                    selected={destination}
-                    setSelected={setDestination}
-                  />
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <Button variant="outline" asChild>
-              <Link href="/">Cancel</Link>
-            </Button>
-            <Button onClick={() => calculateRoute()}>Search Jeepneys</Button>
-          </CardFooter>
-        </Card>
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="outline" asChild>
+                <Link href="/">Cancel</Link>
+              </Button>
+              <Button onClick={() => calculateRoute()}>Search Jeepneys</Button>
+            </CardFooter>
+          </Card>
+          <DavaoWeather className="hidden sm:block" />
+        </div>
       ) : (
         <Card className="border-gray-400">
           <CardHeader className="pb-2 sm:pb-6">
             <CardTitle>Available Routes</CardTitle>
           </CardHeader>
           <CardContent className="pb-4 sm:pb-6">
-            {directionResponse.routes.map((route, index) => (
-              <Card key={index} className="border-gray-400 mb-4">
-                <button onClick={() => setSelectedRoute(index)}>
-                  <CardHeader className="p-4 pb-2">
-                    <CardTitle className="text-xl">Route {index + 1}</CardTitle>
-                  </CardHeader>
-                  <CardContent className="px-4 pb-4">
-                    <div className="flex flex-col text-sm">
-                      <span>
-                        Distance: {route.legs[0]?.distance?.text ?? ""}
-                      </span>
-                      <span>
-                        Duration: {route.legs[0]?.duration?.text ?? ""}
-                      </span>
+            <Accordion type="single" collapsible className="w-full">
+              {directionResponse.routes.map((route, index) => (
+                <AccordionItem key={index} value={index.toString()}>
+                  <AccordionTrigger>
+                    <Card key={index} className="border-gray-400 mb-4">
+                      <button
+                        onClick={() => {
+                          setSelectedRoute(index);
+                          setInstructions(route.legs[0].steps);
+                        }}
+                      >
+                        <CardHeader className="p-4 pb-2">
+                          <CardTitle className="text-xl">
+                            Route {index + 1}
+                          </CardTitle>
+                        </CardHeader>
+                        <CardContent className="px-4 pb-4">
+                          <div className="flex flex-col text-sm">
+                            <span>
+                              Distance: {route.legs[0]?.distance?.text ?? ""}
+                            </span>
+                            <span>
+                              Duration: {route.legs[0]?.duration?.text ?? ""}
+                            </span>
+                          </div>
+                        </CardContent>
+                      </button>
+                    </Card>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="relative before:absolute before:bg-gray-300 before:w-0.5 before:left-4 before:top-0 before:bottom-0">
+                      {instructions?.map((instruction, index) => (
+                        <div key={index} className="mb-4 pl-8 relative">
+                          {instruction.travel_mode === "WALKING" ? (
+                            <div>
+                              <div className="absolute left-2 top-0 w-4 h-4 bg-gray-800 rounded-full border border-white"></div>
+                              <h3 className="text-md font-semibold">WALK</h3>
+                              <p className="text-xs text-gray-500">
+                                About {instruction.duration?.text},{" "}
+                                {instruction.distance?.text}
+                              </p>
+                              <div className="my-2">
+                                {instruction.steps?.map((step, stepIndex) => (
+                                  <div
+                                    key={stepIndex}
+                                    className="flex items-center mb-2"
+                                  >
+                                    <div className="w-3 h-3 rounded-full bg-green-500 mr-2"></div>
+                                    <p
+                                      dangerouslySetInnerHTML={{
+                                        __html: step.instructions,
+                                      }}
+                                    ></p>
+                                  </div>
+                                ))}
+                              </div>
+                              {index === instructions.length - 1 && (
+                                <div>
+                                  <div className="border-t border-gray-400 my-4"></div>
+                                  <h3 className="text-md font-semibold relative">
+                                    <div className="absolute left-[-24px] top-0 w-4 h-4 bg-gray-800 rounded-full border border-white"></div>
+                                    {destination?.name}
+                                  </h3>
+                                  <p
+                                    dangerouslySetInnerHTML={{
+                                      __html: instruction.instructions,
+                                    }}
+                                    className="text-xs text-gray-500"
+                                  ></p>
+                                </div>
+                              )}
+                              <div className="border-t border-gray-400 my-4"></div>
+                            </div>
+                          ) : (
+                            <div>
+                              <div className="absolute left-2 top-0 w-4 h-4 bg-gray-800 rounded-full border border-white"></div>
+                              <div className="flex items-center mb-2">
+                                <Badge className="mr-2">
+                                  {instruction.transit?.line.name}
+                                </Badge>
+                                <p>{instruction.transit?.headsign}</p>
+                              </div>
+                              <div className="border-t border-gray-400 my-4"></div>
+                              <p className="font-semibold relative">
+                                <div className="absolute left-[-24px] top-0 w-4 h-4 bg-gray-800 rounded-full border border-white"></div>
+                                {instruction.transit?.arrival_stop.name}
+                              </p>
+                              <div className="border-t border-gray-400 my-4"></div>
+                            </div>
+                          )}
+                        </div>
+                      ))}
                     </div>
-                  </CardContent>
-                </button>
-              </Card>
-            ))}
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
           </CardContent>
         </Card>
       )}
@@ -144,6 +250,134 @@ export default function Travel() {
         distanceResponse={distanceResponse}
         selectedRoute={selectedRoute}
       />
+      {/* <div>
+        {instructions?.map((instruction, index) => (
+          <div key={index} className="mb-4">
+            {instruction.travel_mode === "WALKING" ? (
+              <div>
+                <h3 className="text-md font-semibold">WALK</h3>
+                <p className="text-xs text-gray-500">
+                  About {instruction.duration?.text},{" "}
+                  {instruction.distance?.text}
+                </p>
+                <div className="pl-5 my-2">
+                  {instruction.steps?.map((step, stepIndex) => (
+                    <div key={stepIndex} className="flex items-center mb-2">
+                      <div className="w-3 h-3 rounded-full bg-green-500 mr-2"></div>
+                      <p
+                        dangerouslySetInnerHTML={{ __html: step.instructions }}
+                      ></p>
+                    </div>
+                  ))}
+                </div>
+                {index === instructions.length - 1 && (
+                  <div>
+                    <div className="border-t border-gray-400 my-4"></div>
+                    <h3 className="text-md font-semibold">
+                      {destination?.name}
+                    </h3>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: instruction.instructions,
+                      }}
+                      className="text-xs text-gray-500"
+                    ></p>
+                  </div>
+                )}
+                <div className="border-t border-gray-400 my-4"></div>
+              </div>
+            ) : (
+              <div>
+                <div className="flex items-center mb-2">
+                  <Badge className="mr-2">
+                    {instruction.transit?.line.name}
+                  </Badge>
+                  <p>{instruction.transit?.headsign}</p>
+                </div>
+                <div className="border-t border-gray-400 my-4"></div>
+                <p className="font-semibold">
+                  {instruction.transit?.arrival_stop.name}
+                </p>
+                <div className="border-t border-gray-400 my-4"></div>
+              </div>
+            )}
+          </div>
+        ))}
+      </div> */}
+
+      {/* <div className="relative before:absolute before:bg-gray-300 before:w-0.5 before:left-4 before:top-0 before:bottom-0">
+        {instructions?.map((instruction, index) => (
+          <div key={index} className="mb-4 pl-8 relative">
+            {instruction.travel_mode === "WALKING" ? (
+              <div>
+                <div className="absolute left-2 top-0 w-4 h-4 bg-gray-800 rounded-full border border-white"></div>
+                <h3 className="text-md font-semibold">WALK</h3>
+                <p className="text-xs text-gray-500">
+                  About {instruction.duration?.text},{" "}
+                  {instruction.distance?.text}
+                </p>
+                <div className="my-2">
+                  {instruction.steps?.map((step, stepIndex) => (
+                    <div key={stepIndex} className="flex items-center mb-2">
+                      <div className="w-3 h-3 rounded-full bg-green-500 mr-2"></div>
+                      <p
+                        dangerouslySetInnerHTML={{ __html: step.instructions }}
+                      ></p>
+                    </div>
+                  ))}
+                </div>
+                {index === instructions.length - 1 && (
+                  <div>
+                    <div className="border-t border-gray-400 my-4"></div>
+                    <h3 className="text-md font-semibold relative">
+                      <div className="absolute left-[-24px] top-0 w-4 h-4 bg-gray-800 rounded-full border border-white"></div>
+                      {destination?.name}
+                    </h3>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: instruction.instructions,
+                      }}
+                      className="text-xs text-gray-500"
+                    ></p>
+                  </div>
+                )}
+                <div className="border-t border-gray-400 my-4"></div>
+              </div>
+            ) : (
+              <div>
+                <div className="absolute left-2 top-0 w-4 h-4 bg-gray-800 rounded-full border border-white"></div>
+                <div className="flex items-center mb-2">
+                  <Badge className="mr-2">
+                    {instruction.transit?.line.name}
+                  </Badge>
+                  <p>{instruction.transit?.headsign}</p>
+                </div>
+                <div className="border-t border-gray-400 my-4"></div>
+                <p className="font-semibold relative">
+                  <div className="absolute left-[-24px] top-0 w-4 h-4 bg-gray-800 rounded-full border border-white"></div>
+                  {instruction.transit?.arrival_stop.name}
+                </p>
+                <div className="border-t border-gray-400 my-4"></div>
+              </div>
+            )}
+          </div>
+        ))}
+      </div> */}
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="absolute bottom-8 right-8 sm:hidden">
+            View Weather
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Davao Current Condition</DialogTitle>
+          </DialogHeader>
+          <div>
+            <DavaoWeather />
+          </div>
+        </DialogContent>
+      </Dialog>
     </main>
   );
 }

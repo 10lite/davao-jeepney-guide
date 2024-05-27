33edb433409d25f804ee6537d7fb83c093874359
aspactores <anakin.pactores@gmail.com>
2024-05-28 06:12:53 +0800

    fix: from and to center issue

--- app/travel/components/map-container.tsx
+++ app/travel/components/map-container.tsx
@@ -1,7 +1,7 @@
 // Components
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import CommuteDetails from './commute-details';
-import { Map } from '@/components/Map';
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import CommuteDetails from "./commute-details";
+import { Map } from "@/components/Map";
 
 interface MapContainerProps {
   source: google.maps.places.PlaceResult | undefined;
@@ -11,36 +11,58 @@ interface MapContainerProps {
   selectedRoute: number | 0;
 }
 
-export default function MapContainer({ source, destination, directionResponse, distanceResponse, selectedRoute}: MapContainerProps ) {
+export default function MapContainer({
+  source,
+  destination,
+  directionResponse,
+  distanceResponse,
+  selectedRoute,
+}: MapContainerProps) {
   return (
-    <Card className='w-full sm:w-[60%] sm:p-0 border-gray-400'>
+    <Card className="w-full sm:w-[60%] sm:p-0 border-gray-400">
       <CardHeader>
         <CardTitle className="flex flex-col gap-1 sm:gap-2 items-center sm:items-start text-xl sm:text-2xl leading-6">
-          {source === undefined && destination === undefined
-            ? 'Input your route to locate jeepney routes' 
-            : 
+          {source === undefined && destination === undefined ? (
+            "Input your route to locate jeepney routes"
+          ) : (
             <>
               <header className="flex flex-col sm:flex-row font-normal gap-1 sm:gap-2">
                 Traveling from
-                <span className='font-semibold underline text-center'>{!source ? 'Source' : source?.name}</span> 
+                <span className="font-semibold underline">
+                  {!source ? "Source" : source?.name}
+                </span>
                 to
-                <span className='font-semibold underline  text-center'>{!destination ? '...' : destination?.name}</span>
+                <span className="font-semibold underline">
+                  {!destination ? "..." : destination?.name}
+                </span>
               </header>
-              {directionResponse &&
-                <CommuteDetails 
-                  distance={directionResponse?.routes[selectedRoute]?.legs[0]?.distance?.text}
-                  duration={directionResponse?.routes[selectedRoute]?.legs[0]?.duration?.text}
+              {directionResponse && (
+                <CommuteDetails
+                  distance={
+                    directionResponse?.routes[selectedRoute]?.legs[0]?.distance
+                      ?.text
+                  }
+                  duration={
+                    directionResponse?.routes[selectedRoute]?.legs[0]?.duration
+                      ?.text
+                  }
                   /* Stops = Number of Transit steps */
-                  stops={directionResponse?.routes[selectedRoute]?.legs[0]?.steps.filter(step => step.travel_mode === 'TRANSIT').length || 0}
+                  stops={
+                    directionResponse?.routes[
+                      selectedRoute
+                    ]?.legs[0]?.steps.filter(
+                      (step) => step.travel_mode === "TRANSIT"
+                    ).length || 0
+                  }
                 />
-              }
+              )}
             </>
-          }
+          )}
         </CardTitle>
       </CardHeader>
-      <CardContent className='p-0'>
-        <div className='h-[20rem] sm:h-[80vh] bg-gray-200 rounded-b-lg'>
-          <Map 
+      <CardContent className="p-0">
+        <div className="h-[20rem] sm:h-[80vh] bg-gray-200 rounded-b-lg">
+          <Map
             source={source}
             destination={destination}
             directionResponse={directionResponse}

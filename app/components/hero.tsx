76bce5b36ec513b557bf1e130812d67227276de5
10lite <rcpaderna@up.edu.ph>
2024-05-28 21:48:50 +0800

    fix: home page top padding fix

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <header className="p-10 flex flex-col-reverse sm:flex-row sm:gap-6 items-center justify-between">
+    <header className="px-10 pb-10 pt-0 flex flex-col-reverse sm:flex-row sm:gap-6 items-center justify-between">
       <section className="flex flex-col gap-2 sm:gap-5 items-center sm:items-start">
         <h1 className="text-4xl sm:text-[4rem] font-bold text-center">Davao Jeepney Guide</h1>
         <p className="text-lg sm:text-2xl text-center leading-5">
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-8 sm:gap-[5rem] sm:pt-[5%] bg-gray-100">
+    <main className="flex min-h-screen flex-col items-center gap-8 sm:gap-[5rem] sm:pt-[2.5%] xl:pt-[5%] bg-gray-100">
       <Hero />
       <Features />
     </main>
